4fa81ebf38afd6fab0a784b8e7dc9d876b4895fb
Sebastian Meragelman <sebastianmeragelman@gmail.com>
2026-06-24 07:53:50 -0300

    creo estructura inicial de directorios

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+# app/__init__.py
+
+#----------#
+#INICIALIZAR FLASK
+#----------#
+
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    from app.routes import main
+    app.register_blueprint(main)
+
+    return app
